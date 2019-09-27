8aedee11b26bfa2be4e6d8cffdd70f7d353a21f1
Khondokar Hashibul Islam <peeyalk@hotmail.com>
2019-09-27 17:38:44 +0900

    Updates

---
+++ precache-manifest.36a24e62e9342797bec55a4222d02f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77075b9f93ee5b3c78474716ebb9aa2b",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "311c79c7f978dffb82db",
+    "url": "/calculator-react/static/css/2.5e5220fe.chunk.css"
+  },
+  {
+    "revision": "b60c1568a8017e320b38",
+    "url": "/calculator-react/static/css/main.90921087.chunk.css"
+  },
+  {
+    "revision": "311c79c7f978dffb82db",
+    "url": "/calculator-react/static/js/2.f24e6af8.chunk.js"
+  },
+  {
+    "revision": "b60c1568a8017e320b38",
+    "url": "/calculator-react/static/js/main.b17b239a.chunk.js"
+  },
+  {
+    "revision": "823ceae628c968c44b0a",
+    "url": "/calculator-react/static/js/runtime~main.b9732b27.js"
+  }
+]);
