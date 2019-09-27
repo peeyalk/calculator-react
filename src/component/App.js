2a50c2ab84733a9d22b86b3da3f709a6c7bb5c76
Khondokar Hashibul Islam <peeyalk@hotmail.com>
2019-09-27 16:24:26 +0900

    added keyboard key press feature

--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,7 @@ import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
+import KeyboardEventHandler from "react-keyboard-event-handler";
 
 export default class App extends React.Component {
   state = {
@@ -15,11 +16,61 @@ export default class App extends React.Component {
     this.setState(calculate(this.state, buttonName));
   };
 
+  handleKeyPress = (key, event) => {
+    //console.log(event.key);
+
+    const validKey = [
+      "c",
+      "+",
+      "+/-",
+      "%",
+      "/",
+      "x",
+      "*",
+      "-",
+      "=",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      ".",
+      "=",
+      "Enter",
+    ];
+
+    try {
+      if (validKey.indexOf(event.key) !== -1) {
+        if (event.key === "c") {
+          this.handleClick("AC");
+        } else if (event.key === "/") {
+          this.handleClick("÷");
+        } else if (event.key === "*") {
+          this.handleClick("x");
+        } else if (event.key === "Enter") {
+          this.handleClick("=");
+        } else {
+          this.handleClick(event.key);
+        }
+      }
+    } catch (error) {}
+  };
+
   render() {
     return (
       <div className="component-app">
         <Display value={this.state.next || this.state.total || "0"} />
-        <ButtonPanel clickHandler={this.handleClick} />
+        <KeyboardEventHandler
+            handleKeys={["all"]}
+            onKeyEvent={this.handleKeyPress}
+          />
+        <ButtonPanel clickHandler={this.handleClick}>
+        </ButtonPanel>
       </div>
     );
   }
